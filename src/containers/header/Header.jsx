f793b76fe10be0c50c4154fc151169022230c320
DraRoVol <107344569+DraRoVol@users.noreply.github.com>
2023-06-01 13:35:57 +0300

    update

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import picture from '../../assets/picture1.webp'
+import "./header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="gem__header section__padding" id="home">
+      <div className="gem__header-content">
+        <h1 className="gradient__text">Let's Build amazing with Viki OpenAI</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis
+          obcaecati eum iusto numquam nulla maxime, a iure tenetur facere quo.
+          Nisi maxime voluptatum veritatis voluptate voluptas nihil eos quam
+          amet!
+        </p>
+        <div className="gem__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className='gem__header-content__people'>
+          <img src='' alt="people" />
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        </div>
+        <div className='gem__header-image'>
+          <img src={picture} alt='illustration' />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
